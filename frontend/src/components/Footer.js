4191f6bad12ec512f9307234ba6482d2ce8037eb
rhythm298 <rhythmmehta29@gmail.com>
2025-04-10 19:36:29 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -13,6 +13,12 @@ const Footer = () => {
             <a href="#"><i className="fab fa-facebook"></i></a>
             <a href="#"><i className="fab fa-instagram"></i></a>
             <a href="#"><i className="fab fa-pinterest"></i></a>
+            <a href="/about">About</a>
+            <a href="/privacy">Privacy</a>
+            <a href="/terms">Terms</a>
+    <button onClick={() => {}} className="text-blue-500 underline">About</button>
+
+
           </div>
         </div>
         <div className="footer-section">

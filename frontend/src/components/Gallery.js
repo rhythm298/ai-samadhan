58512daa040f832abf15c69993da693dbf93fabd
rhythm298 <rhythmmehta29@gmail.com>
2025-04-10 19:11:30 +0530

    Create Gallery.js

---
+++ frontend/src/components/Gallery.js
@@ -0,0 +1,13 @@
+// src/components/Gallery.js
+import React from 'react';
+import './Button.css';
+
+const Gallery = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <button className="futuristic-button">View Gallery</button>
+    </div>
+  );
+};
+
+export default Gallery;

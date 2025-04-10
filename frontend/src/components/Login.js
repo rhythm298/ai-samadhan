021afdc39b8167c5a44d041edf0f6b8f86b50117
rhythm298 <rhythmmehta29@gmail.com>
2025-04-10 19:11:48 +0530

    Create Login.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,13 @@
+// src/components/Login.js
+import React from 'react';
+import './Button.css';
+
+const Login = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <button className="futuristic-button">Login</button>
+    </div>
+  );
+};
+
+export default Login;

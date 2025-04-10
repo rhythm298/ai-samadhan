3dad9253f7a037ac82084e9411cd374f3a753eda
rhythm298 <rhythmmehta29@gmail.com>
2025-04-10 19:18:14 +0530

    Create Register.js

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,13 @@
+// src/components/Register.js
+import React from 'react';
+import './Button.css';
+
+const Register = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <button className="futuristic-button">Register</button>
+    </div>
+  );
+};
+
+export default Register;
